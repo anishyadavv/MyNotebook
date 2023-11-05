b1631734ad88af17bac6a05f8653b89e5a1cdd56
anish <anishyadav7853@gmail.com>
2023-11-05 23:44:13 +0530

    changed mongo uri

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const express = require('express');
-var cors = require('cors');
+const mongoose = require("mongoose");
+const express = require("express");
+var cors = require("cors");
 
 const app = express();
 require("dotenv").config();
 const URI = process.env.MONGOURI;
-mongoose.connect("mongodb+srv://anish123:anish@cluster0.wrim3.mongodb.net/notebook");
+const PORT = process.env.PORT || 5000;
+mongoose.connect(URI);
 
+app.use(cors());
+app.use(express.json()); //this is a middleware to use req.body
 
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
-
-app.use(cors())
-app.use(express.json());//this is a middleware to use req.body 
-
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'));
-
-app.get('/',(req,res)=>{
-    res.send('Hello Anish');
+app.get("/", (req, res) => {
+  res.send("Hello Anish");
 });
-app.listen(5000,()=>{console.log('listening on port 5000');});
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+});
